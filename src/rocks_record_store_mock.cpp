d4769e4d82b8558bf21bf58c7bc7e8c44fedff50
Igor Canadi <icanadi@fb.com>
2016-11-08 11:56:01 -0800

    SERVER-26685 Tie oplog visibility to durability (RocksDB version)

--- src/rocks_record_store_mock.cpp
+++ src/rocks_record_store_mock.cpp
@@ -29,13 +29,24 @@
 
 #include "mongo/platform/basic.h"
 
+#include "mongo/base/init.h"
+#include "mongo/db/namespace_string.h"
+#include "mongo/db/service_context.h"
+#include "mongo/db/service_context_noop.h"
+#include "mongo/stdx/memory.h"
+
 #include "rocks_engine.h"
 
 namespace mongo {
 
-    // static
-    bool RocksEngine::initRsOplogBackgroundThread(StringData ns) {
-        return false;
-    }
+// static
+bool RocksEngine::initRsOplogBackgroundThread(StringData ns) {
+    return NamespaceString::oplog(ns);
+}
+
+MONGO_INITIALIZER(SetGlobalEnvironment)(InitializerContext* context) {
+    setGlobalServiceContext(stdx::make_unique<ServiceContextNoop>());
+    return Status::OK();
+}
 
 }  // namespace mongo
