ecde09b32bae99e0c94d000c8ca97382e31df605
Roy Reznik <royrez@microsoft.com>
2017-03-04 12:05:14 +0200

    Fix compilation on RocksDB < 4.13

--- src/rocks_parameters.cpp
+++ src/rocks_parameters.cpp
@@ -43,6 +43,7 @@
 #include <rocksdb/db.h>
 #include <rocksdb/convenience.h>
 #include <rocksdb/options.h>
+#include <rocksdb/version.h>
 
 namespace mongo {
 
@@ -196,6 +197,7 @@ namespace mongo {
     }
 
     Status RocksOptionsParameter::setFromString(const std::string& str) {
+#if defined(ROCKSDB_MAJOR) && (ROCKSDB_MAJOR > 4 || (ROCKSDB_MAJOR == 4 && ROCKSDB_MINOR >= 13))        
         log() << "RocksDB: Attempting to apply settings: " << str;
         
         std::unordered_map<std::string, std::string> optionsMap;
@@ -210,6 +212,9 @@ namespace mongo {
         }
         
         return Status::OK();
+#else
+        return Status(ErrorCodes::BadValue, "This action is supported for RocksDB 4.13 and up");
+#endif
     }
 
     RocksTicketServerParameter::RocksTicketServerParameter(TicketHolder* holder, const std::string& name)
