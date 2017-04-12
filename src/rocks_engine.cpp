915e05319630bce884cae8b3641b24019b34cc26
Igor Canadi <igor@rockset.io>
2017-04-12 12:46:59 -0700

    SERVER-28546 confirm RocksDB unindex operations remove the correct document

--- src/rocks_engine.cpp
+++ src/rocks_engine.cpp
@@ -196,7 +196,7 @@ namespace mongo {
         private:
             const RocksEngine* _engine;
         };
-        
+
         // ServerParameter to limit concurrency, to prevent thousands of threads running
         // concurrent searches and thus blocking the entire DB.
         class RocksTicketServerParameter : public ServerParameter {
@@ -229,7 +229,7 @@ namespace mongo {
 
                 return _holder->resize(newNum);
             }
-            
+
             TicketHolder* _holder;
         };
 
@@ -465,7 +465,8 @@ namespace mongo {
         RocksIndexBase* index;
         if (desc->unique()) {
             index = new RocksUniqueIndex(_db.get(), prefix, ident.toString(),
-                                         Ordering::make(desc->keyPattern()), std::move(config));
+                                         Ordering::make(desc->keyPattern()), std::move(config),
+                                         desc->isPartial());
         } else {
             auto si = new RocksStandardIndex(_db.get(), prefix, ident.toString(),
                                              Ordering::make(desc->keyPattern()), std::move(config));
--- src/rocks_index.h
+++ src/rocks_index.h
@@ -100,7 +100,7 @@ namespace mongo {
     class RocksUniqueIndex : public RocksIndexBase {
     public:
         RocksUniqueIndex(rocksdb::DB* db, std::string prefix, std::string ident, Ordering order,
-                         const BSONObj& config);
+                         const BSONObj& config, bool partial = false);
 
         virtual Status insert(OperationContext* txn, const BSONObj& key, const RecordId& loc,
                               bool dupsAllowed);
@@ -113,6 +113,8 @@ namespace mongo {
 
         virtual SortedDataBuilderInterface* getBulkBuilder(OperationContext* txn,
                                                            bool dupsAllowed) override;
+    private:
+        const bool _partial;
     };
 
     class RocksStandardIndex : public RocksIndexBase {
