07ecab9e85ff3863acdfb87286d103efb1e60a6c
Denis Protivenskii <denis.protivenskii@percona.com>
2017-05-11 17:41:52 +0300

    PSMDB-139 Compact prefix 0 when compacting dropped prefix markers
    Prefix 0 contains metadata that may also need compaction from time
    to time.

--- src/rocks_util.cpp
+++ src/rocks_util.cpp
@@ -35,6 +35,11 @@
 #include "mongo/platform/endian.h"
 
 namespace mongo {
+    std::string encodePrefix(uint32_t prefix) {
+        uint32_t bigEndianPrefix = endian::nativeToBig(prefix);
+        return std::string(reinterpret_cast<const char*>(&bigEndianPrefix), sizeof(uint32_t));
+    }
+
     // we encode prefixes in big endian because we want to quickly jump to the max prefix
     // (iter->SeekToLast())
     bool extractPrefix(const rocksdb::Slice& slice, uint32_t* prefix) {
