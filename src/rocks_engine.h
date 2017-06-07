e2254cb75ca3e162c5f3c217b95871ea974248f6
Denis Protivenskii <denis.protivenskii@percona.com>
2017-06-07 19:40:28 +0300

    PSMDB-127 Implementation of compaction on the background thread (#76)
    * PSMDB-127 pass RocksCompactionScheduler to RocksRecordStore
    
    (cherry picked from commit f45122466b5bfa94e4e02694e14a8679a43d6a45)
    
    * PSMDB-127 implementation of compaction thread
    
    (cherry picked from commit 276ac71217a7e71626863748b657d8e65c2c59c1)
    
    * PSMDB-127 reroute compactions to compaction thread
    
    (cherry picked from commit 9acf066c168ab524eed369108833d1aaee7e086c)
    
    * PSMDB-127 call CancelAllBackgroundWork to stop manual compactions running in background
    
    (cherry picked from commit 9d8566595445945ff6795607738ca7dc88d3475d)
    
    * PSMDB-127 add callback for _droppedPrefixes cleanup
    
    (cherry picked from commit 4a5351569e73f4d9728c038ab365d9c8ba8f1075)
    
    * PSMDB-127 ignore snapshots in PrefixDeletingCompactionFilter
    
    (cherry picked from commit 21577d1d4b93b7d440b1e445bf09dff4b7f7ec5d)
    
    * PSMDB-127 delete dropped prefix marker at the end of compaction
    
    (cherry picked from commit 03dc5f16d35d7cd0423e0f57e345d21f5df7b428)
    
    * PSMDB-127 remove prefix alive check and do compaction of orphaned dropped prefixes unconditionally
    
    (cherry picked from commit 64515d1f2985037d415dff18ef8161bf686c136e)
    
    * PSMDB-127 fix unittests according to changes in RocksRecordStore
    
    (cherry picked from commit 78789475e24feb6b7123b8ba07c53f5b4bbeec5d)
    
    * PSMDB-127 CancelAllBackgroundWork if RocksDB >= 4.13

--- src/rocks_engine.h
+++ src/rocks_engine.h
@@ -154,6 +154,8 @@ namespace mongo {
 
         RocksTransactionEngine* getTransactionEngine() { return &_transactionEngine; }
 
+        RocksCompactionScheduler* getCompactionScheduler() const { return _compactionScheduler.get(); }
+
         int getMaxWriteMBPerSec() const { return _maxWriteMBPerSec; }
         void setMaxWriteMBPerSec(int maxWriteMBPerSec);
 
