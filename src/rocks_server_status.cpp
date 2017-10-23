d1ac58583c777a2903ba3d0339ceb53f5b55917f
Igor Canadi <igor@rockset.io>
2017-10-23 18:59:33 +0000

    SERVER-31681: Fix compile, tnx -> opCtx

--- src/rocks_server_status.cpp
+++ src/rocks_server_status.cpp
@@ -70,7 +70,7 @@ namespace mongo {
 
     BSONObj RocksServerStatusSection::generateSection(OperationContext* opCtx,
                                                       const BSONElement& configElement) const {
-        Lock::GlobalLock lk(txn->lockState(), LockMode::MODE_IS, UINT_MAX);
+        Lock::GlobalLock lk(opCtx->lockState(), LockMode::MODE_IS, UINT_MAX);
 
         BSONObjBuilder bob;
 
